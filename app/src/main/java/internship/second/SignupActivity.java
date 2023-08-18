f64f42083e0899a75ca8d57c8fc3e770a8480084
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-18 13:30:08 +0530

    18th Aug

--- app/src/main/java/internship/second/SignupActivity.java
+++ app/src/main/java/internship/second/SignupActivity.java
@@ -2,13 +2,84 @@ package internship.second;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SignupActivity extends AppCompatActivity {
 
+    Button login,signup;
+    EditText name,email,contact,password,confirmPassword;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        login = findViewById(R.id.signup_login);
+
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+        contact = findViewById(R.id.signup_contact);
+        confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        signup = findViewById(R.id.signup_signup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().trim().equals("")){
+                    name.setError("Name Required");
+                }
+                else if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(contact.getText().toString().trim().equals("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().trim().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().equals("")){
+                    confirmPassword.setError("Confirm Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().length()<6){
+                    confirmPassword.setError("Min. 6 Char Confirm Password Required");
+                }
+                else if(!confirmPassword.getText().toString().trim().matches(password.getText().toString())){
+                    confirmPassword.setError("Confirm Password Does Not Match");
+                }
+                else {
+                    System.out.println("Signup Successfully " + email.getText().toString() + password.getText().toString());
+                    Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(view, "Signup Successfully", Snackbar.LENGTH_SHORT).show();
+                    onBackPressed();
+                }
+            }
+        });
+        
     }
 }
